ed984f04a968b44da53db79d1b12a05605d926a5
chanchal-chauhan99 <144597968+dhruvapanwar@users.noreply.github.com>
2024-06-10 13:41:16 +0530

    swapped value

---
+++ program1.py
@@ -0,0 +1,11 @@
+# Prompt the user to input two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Swap the values
+number1, number2 = number2, number1
+
+# Print the swapped values
+print("After swapping:")
+print(f"First number: {number1}")
+print(f"Second number: {number2}")
